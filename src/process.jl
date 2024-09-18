3f59a0312d20861fc564dd513dfbbe684bfe3f81
benyurek <149546012+benyurek@users.noreply.github.com>
2024-09-18 12:02:51 -0700

    Update process.jl

--- src/process.jl
+++ src/process.jl
@@ -297,8 +297,9 @@ function proc_discrete(x_locs,y_locs,raw_image,mask_image;Np=33,widx=129,widy=wi
 
 
     # iterates over each of the tiles in an image
-    @threads for jx in 1:tilex
-        @threads for jy in 1:tiley
+    Threads.@threads for jx in 1:tilex
+        for jy in 1:tiley
+
         
             # calculate the x/y ranges and star indices in the tile
             xrng, yrng, star_ind = im_subrng(jx,jy,cx,cy,sx0+2,sy0+2,px0,py0,stepx,stepy,padx,pady,tilex,tiley)
